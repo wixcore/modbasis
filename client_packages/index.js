01f8a0293d66936e4986c9d3c31c6e5f76bca6fc
Mykola Dovhopol <97382411+lifesheets@users.noreply.github.com>
2023-02-24 15:28:15 +0200

    [Add] Вимикаємо фоновий шум. Сирену, навантажувач, гудки та інше.

--- client_packages/index.js
+++ client_packages/index.js
@@ -11,3 +11,8 @@ require('./game_resources/handlers/wixcore/configs');
 require('./game_resources/handlers/wixcore/methods');
 require('./game_resources/handlers/wixcore/shareds');
 require('./game_resources/handlers/wixcore/modules');
+
+mp.events.add('playerReady', () => {
+  // Вимикаємо фоновий шум. Сирену, навантажувач, гудки та інше.
+  mp.game.audio.startAudioScene('CHARACTER_CHANGE_IN_SKY_SCENE');
+});
